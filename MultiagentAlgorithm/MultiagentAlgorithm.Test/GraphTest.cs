e28674c9ac986917d182fe78dfd96bbf3a6aba2c
drazenp <drazenp86@gmail.com>
2016-03-19 08:19:31 +0100

    Implemeted basic graph initialization. First line is read. Dummy graph is included.

---
+++ MultiagentAlgorithm/MultiagentAlgorithm.Test/GraphTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MultiagentAlgorithm.Test
+{
+    [TestClass]
+    public class GraphTest
+    {
+        [TestMethod]
+        public void Graph_FirstLineRead_Sucess()
+        {
+            var loaderMock = new Mock<IDataLoader>();
+            loaderMock.Setup(m => m.LoadData()).Returns(new List<string>() { "7 11 011" } );
+
+            var graph = new Graph(loaderMock.Object);
+
+            graph.InitializeGraph();
+
+            Assert.AreEqual(7, graph.VerticesWeights.Length, "The number of vertices weights is not correct.");
+            Assert.AreEqual(11, graph.NumberOfEdges, "The number of edges is not correct.");
+        }
+    }
+}
---
+++ MultiagentAlgorithm/MultiagentAlgorithm/IDataLoader.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MultiagentAlgorithm
+{
+    public interface IDataLoader
+    {
+        IEnumerable<string> LoadData();
+    }
+}
