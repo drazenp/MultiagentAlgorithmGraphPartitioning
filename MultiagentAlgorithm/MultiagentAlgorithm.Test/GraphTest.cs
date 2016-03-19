bc0652d257d6d49c42814e970cc1036c5919183b
drazenp <drazenp86@gmail.com>
2016-03-19 08:34:36 +0100

    Initialized vertices weights.

--- MultiagentAlgorithm/MultiagentAlgorithm.Test/GraphTest.cs
+++ MultiagentAlgorithm/MultiagentAlgorithm.Test/GraphTest.cs
@@ -7,11 +7,20 @@ namespace MultiagentAlgorithm.Test
     [TestClass]
     public class GraphTest
     {
+        private readonly List<string> _dummyFile = new List<string>() { "7 11 011",
+                                                                        "4 5 1 3 2 2 1",
+                                                                        "2 1 1 3 2 4 1",
+                                                                        "5 5 3 4 2 2 2 1 2",
+                                                                        "3 2 1 3 2 6 2 7 5",
+                                                                        "1 1 1 3 3 6 2",
+                                                                        "6 5 2 4 2 7 6",
+                                                                        "2 6 6 4 5" };
+            
         [TestMethod]
         public void Graph_FirstLineRead_Sucess()
         {
             var loaderMock = new Mock<IDataLoader>();
-            loaderMock.Setup(m => m.LoadData()).Returns(new List<string>() { "7 11 011" } );
+            loaderMock.Setup(m => m.LoadData()).Returns(_dummyFile);
 
             var graph = new Graph(loaderMock.Object);
 
@@ -19,6 +28,25 @@ namespace MultiagentAlgorithm.Test
 
             Assert.AreEqual(7, graph.VerticesWeights.Length, "The number of vertices weights is not correct.");
             Assert.AreEqual(11, graph.NumberOfEdges, "The number of edges is not correct.");
+        }
+
+        [TestMethod]
+        public void Graph_VerticesWeights_Initalized()
+        {
+            var loaderMock = new Mock<IDataLoader>();
+            loaderMock.Setup(m => m.LoadData()).Returns(_dummyFile);
+
+            var graph = new Graph(loaderMock.Object);
+
+            graph.InitializeGraph();
+
+            Assert.AreEqual(4, graph.VerticesWeights[0]);
+            Assert.AreEqual(2, graph.VerticesWeights[1]);
+            Assert.AreEqual(5, graph.VerticesWeights[2]);
+            Assert.AreEqual(3, graph.VerticesWeights[3]);
+            Assert.AreEqual(1, graph.VerticesWeights[4]);
+            Assert.AreEqual(6, graph.VerticesWeights[5]);
+            Assert.AreEqual(2, graph.VerticesWeights[6]);
         }
     }
 }
