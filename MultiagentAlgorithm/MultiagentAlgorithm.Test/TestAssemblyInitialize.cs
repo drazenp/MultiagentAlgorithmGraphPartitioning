d60a20dbb8ed8e338f0a798ceb8b97947430d4c9
drazenp <drazenp86@gmail.com>
2016-03-25 20:23:33 +0100

    Small refactoring the test classes.

---
+++ MultiagentAlgorithm/MultiagentAlgorithm.Test/TestAssemblyInitialize.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiagentAlgorithm.Test
+{
+    [TestClass]
+    public static class TestAssemblyInitialize
+    {
+        [AssemblyInitialize]
+        public static void Configure(TestContext tc)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+    }
+}
