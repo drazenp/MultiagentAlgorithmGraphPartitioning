c5fea9ddfc9019f0d040572c8a94931d01e21df5
drazenp <drazenp86@gmail.com>
2016-06-02 07:09:55 +0200

    Updated script.

--- MultiagentAlgorithm/MultiAgentAlgorithmAnalyze/db/script.sql
+++ MultiagentAlgorithm/MultiAgentAlgorithmAnalyze/db/script.sql
@@ -1,36 +1,60 @@
 ﻿	/* Initialize graph file paths.*/
     create table _GraphFilePaths(GraphFilePath nvarchar(150), NumberOfPartitions integer, ColoringProbability float, MovingProbability float, NumberOfVerticesForBalance integer, TimesToRun integer);
-    insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('myciel3.col', 2, 0.9, 0.85, 6, 20);
+    --insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('myciel3.col', 2, 0.9, 0.85, 6, 20);
+	/* Group two */
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('C2000.5.col', 125, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('C4000.5.col', 250, 0.9, 0.85, 12, 20);
+	/* Group one */
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('DSJC1000.1.col', 63, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('DSJC1000.5.col', 63, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('flat1000_50_0.col', 63, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('flat1000_60_0.col', 63, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('flat1000_76_0.col', 63, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('DSJC125.1.col', 8, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('jean.col', 5, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('le450_15a.col', 29, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('le450_15b.col', 29, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('le450_15c.col', 29, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('le450_15d.col', 29, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('le450_25a.col', 29, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('le450_25b.col', 29, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('le450_25c.col', 29, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('le450_25d.col', 29, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('le450_5a.col', 29, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('le450_5b.col', 29, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('le450_5c.col', 29, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('miles1000.col', 8, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('miles1500.col', 8, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('miles500.col', 8, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('miles750.col', 8, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('mulsol.i.1.col', 13, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('mulsol.i.5.col', 12, 0.9, 0.85, 12, 20);
 	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('myciel4.col', 2, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('myciel5.col', 3, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('myciel7.col', 12, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('queen12_12.col', 9, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('queen16_16.col', 16, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('queen5_5.col', 2, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('queen8_12.col', 6, 0.9, 0.85, 12, 20);
+	insert into  _GraphFilePaths (GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun) values ('queen8_8.col', 4, 0.9, 0.85, 12, 20);
 
-	/* Initialize number of ants.*/
-	create table _NumberOfAnts(NumberOfAnts integer);
-	insert into _NumberOfAnts(NumberOfAnts)values(1);
-	insert into _NumberOfAnts(NumberOfAnts)values(2);
-	insert into _NumberOfAnts(NumberOfAnts)values(3);
-	insert into _NumberOfAnts(NumberOfAnts)values(4);
-	insert into _NumberOfAnts(NumberOfAnts)values(5);
-	insert into _NumberOfAnts(NumberOfAnts)values(6);
-	insert into _NumberOfAnts(NumberOfAnts)values(7);
-	insert into _NumberOfAnts(NumberOfAnts)values(8);
 
-	create table _NumberOfIterations(NumberOfIterations integer);
-	insert into _NumberOfIterations(NumberOfIterations)values(100);
-	insert into _NumberOfIterations(NumberOfIterations)values(500);
-	insert into _NumberOfIterations(NumberOfIterations)values(1000);
-	insert into _NumberOfIterations(NumberOfIterations)values(3000);
-	insert into _NumberOfIterations(NumberOfIterations)values(5000);
+	/* Initialize number of ants and iterations.*/
+	create table _NumberOfAntsAndIterations(NumberOfAnts integer, NumberOfIterations integer);
+	insert into _NumberOfAntsAndIterations(NumberOfAnts, NumberOfIterations)values(3, 900);
+	insert into _NumberOfAntsAndIterations(NumberOfAnts, NumberOfIterations)values(4, 800);
+	insert into _NumberOfAntsAndIterations(NumberOfAnts, NumberOfIterations)values(5, 700);
+	insert into _NumberOfAntsAndIterations(NumberOfAnts, NumberOfIterations)values(6, 600);
+	insert into _NumberOfAntsAndIterations(NumberOfAnts, NumberOfIterations)values(7, 500);
+	insert into _NumberOfAntsAndIterations(NumberOfAnts, NumberOfIterations)values(8, 400);
+	insert into _NumberOfAntsAndIterations(NumberOfAnts, NumberOfIterations)values(9, 300);
 
-	select *
-	from _GraphFilePaths, _NumberOfAnts, _NumberOfIterations
-	
 	delete from AnalyzeData;
 	delete from AnalyzeResults;
 	
-	/*GraphFilePath, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, TimesToRun*/
 	insert into AnalyzeData(GraphFilePath, NumberOfAnts, NumberOfPartitions, ColoringProbability, MovingProbability, NumberOfVerticesForBalance, NumberOfIterations, TimesToRun)
-	select g.GraphFilePath, a.NumberOfAnts, g.NumberOfPartitions, g.ColoringProbability, g.MovingProbability, g.NumberOfVerticesForBalance, i.NumberOfIterations, g.TimesToRun
-	from _GraphFilePaths as g, _NumberOfAnts as a, _NumberOfIterations as i;
+	select g.GraphFilePath, ai.NumberOfAnts, g.NumberOfPartitions, g.ColoringProbability, g.MovingProbability, g.NumberOfVerticesForBalance, ai.NumberOfIterations, g.TimesToRun
+	from _GraphFilePaths as g, _NumberOfAntsAndIterations as ai
 
     drop table _GraphFilePaths;
 	drop table _NumberOfAnts;
